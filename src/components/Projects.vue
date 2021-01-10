73b6a0823b2a295fb8e2d3847af8ffce40d15c01
karahman <jakarahman007@gmail.com>
2021-01-10 22:15:48 +0700

    edit image style

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -21,7 +21,7 @@
             @click="emitShowScreenshotsEvent(project.images)"
           >
             <!-- Screenshot -->
-            <img class="object-cover" :src="project.images[0]" />
+            <img class="object-cover min-w-full" :src="project.images[0]" />
 
             <div class="px-5 py-2">
               <!-- Tags -->
